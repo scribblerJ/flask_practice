1b0612095ff67ddfd266e5471ac38e0b7f193e24
scribblerJ <yjg950324@naver.com>
2022-09-15 11:20:52 +0900

    FEAT: Add basic navigator

--- server.py
+++ server.py
@@ -1,19 +1,40 @@
-from flask import Flask
+from flask import Flask 
 import random
 
 app = Flask(__name__)
 
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is....'},
+    {'id': 2, 'title': 'css', 'body': 'html is....'},
+    {'id': 3, 'title': 'javascript', 'body': 'html is....'}
+]
+navigator = '<ol>'+''.join([f'<li><a href="/read/{topic["id"]}/">{topic["title"]}<a></li>' for topic in topics])+'</ol>'
+header = '<h1><a href="/">WEB</a></h1>' + navigator
+
 @app.route('/')         # 접속한 URI가 '/' 즉 root 라면
 def index():            # 이하 함수를 실행한다
-    return 'Index Page'
+    return f'''<!doctype html>
+    <html>
+        <body>
+           {header}
+            <h2>Welcome</h2>
+            Hello, Web
+        </body>
+    </html>
+    '''
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
-@app.route('/number/<num>/')        # URL 중 변수로 받고 싶은 부분을 중괄호(<>)로 표기
-def number_printer(num):            # 동명의 인자를 함수로 받아서 사용
-    return 'The number is '+ num
+@app.route('/read/<id>/')        # URL 중 변수로 받고 싶은 부분을 중괄호(<>)로 표기
+def number_printer(id):          # 동명의 인자를 함수로 받아서 사용
+    topic = [i for i in topics if i['id'] == int(id)][0]
+    return f'''<!doctype html>
+    <html>
+        <body>
+            {header}
+            <h2>{topic["title"]}</h2>
+            <p>{topic["body"]}</p>
+        </body>
+    </html>
+    '''
 
 app.run(debug=True)
 
