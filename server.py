1bae8bd8b047419c94b24da6630675bc1a42537b
scribblerJ <yjg950324@naver.com>
2022-09-17 01:35:39 +0900

    REFACTOR: Make unified html template into a function named template

--- server.py
+++ server.py
@@ -1,40 +1,34 @@
 from flask import Flask 
-import random
 
 app = Flask(__name__)
 
 topics = [
     {'id': 1, 'title': 'html', 'body': 'html is....'},
-    {'id': 2, 'title': 'css', 'body': 'html is....'},
-    {'id': 3, 'title': 'javascript', 'body': 'html is....'}
+    {'id': 2, 'title': 'css', 'body': 'css is....'},
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is....'}
 ]
 navigator = '<ol>'+''.join([f'<li><a href="/read/{topic["id"]}/">{topic["title"]}<a></li>' for topic in topics])+'</ol>'
 header = '<h1><a href="/">WEB</a></h1>' + navigator
 
-@app.route('/')         # 접속한 URI가 '/' 즉 root 라면
-def index():            # 이하 함수를 실행한다
+def template(title, body):
     return f'''<!doctype html>
     <html>
         <body>
            {header}
-            <h2>Welcome</h2>
-            Hello, Web
+            <h2>{title}</h2>
+            {body}
         </body>
     </html>
     '''
 
+@app.route('/')         # 접속한 URI가 '/' 즉 root 라면
+def index():            # 이하 함수를 실행한다
+    return template('Welcome', 'Hello, Web')
+
 @app.route('/read/<id>/')        # URL 중 변수로 받고 싶은 부분을 중괄호(<>)로 표기
 def number_printer(id):          # 동명의 인자를 함수로 받아서 사용
     topic = [i for i in topics if i['id'] == int(id)][0]
-    return f'''<!doctype html>
-    <html>
-        <body>
-            {header}
-            <h2>{topic["title"]}</h2>
-            <p>{topic["body"]}</p>
-        </body>
-    </html>
-    '''
+    return template(topic['title'], topic['body'])
 
 app.run(debug=True)
 
