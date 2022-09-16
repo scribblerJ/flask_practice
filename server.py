b79189de5963b86adfbbdad503a29b9bba4fa7f8
scribblerJ <yjg950324@naver.com>
2022-09-17 02:05:09 +0900

    FEAT: make creating new ccontent function

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
@@ -7,17 +7,22 @@ topics = [
     {'id': 2, 'title': 'css', 'body': 'css is....'},
     {'id': 3, 'title': 'javascript', 'body': 'javascript is....'}
 ]
-navigator = '<ol>'+''.join([f'<li><a href="/read/{topic["id"]}/">{topic["title"]}<a></li>' for topic in topics])+'</ol>'
-header = '<h1><a href="/">WEB</a></h1>' + navigator
+next_topic_id = 4
+
 
 def template(title, body):
+    navigator = '<ol>'+''.join([f'<li><a href="/read/{topic["id"]}/">{topic["title"]}<a></li>' for topic in topics])+'</ol>'
     return f'''<!doctype html>
     <html>
         <body>
-           {header}
+           <h1><a href="/">WEB</a></h1>
+            {navigator}
             <h2>{title}</h2>
             {body}
         </body>
+        <footer>
+            <a href="/create/">create</a>
+        </footer>
     </html>
     '''
 
@@ -29,6 +34,24 @@ def index():            # 이하 함수를 실행한다
 def number_printer(id):          # 동명의 인자를 함수로 받아서 사용
     topic = [i for i in topics if i['id'] == int(id)][0]
     return template(topic['title'], topic['body'])
+
+@app.route('/create/', methods=['GET', 'POST'])          # 
+def create():
+    if request.method == 'GET':
+        content = '''
+            <form action="/create/" method="POST">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit" value="create"><p>
+            </form>
+        '''
+        return template('create', content)
+    else:
+        global next_topic_id
+        topics.append({'id': next_topic_id, 'title': request.form['title'], 'body': request.form['body']})
+        next_topic_id += 1
+        return redirect(f'/read/{str(next_topic_id-1)}/')
+        # return topics
 
 app.run(debug=True)
 
